0a992faff2b22a15d3346c4229f7093ba471ee5c
yuler <joyful4yu@gmail.com>
2019-09-22 19:04:55 +0800

    feat: add get default auth & config util

---
+++ src/util/config/get-default.ts
@@ -0,0 +1,56 @@
+export const getDefaultConfig = async (existingCopy: object) => {
+  let migrated = false
+
+  const config: {
+    _: string
+    [key: string]: any
+  } = {
+    _: 'This is 🐟 cli config file. For more information set: https://github.com/yuler/cli'
+  }
+
+  if (existingCopy) {
+    const keep = [
+      '_',
+      'cos'
+    ]
+
+    try {
+      const existing = Object.assign({}, existingCopy)
+      Object.assign(config, existing)
+
+      for (const key in Object.keys(config)) {
+        if (!keep.includes(key)) {
+          delete config[key]
+        }
+      }
+
+    } catch (err) {}
+  }
+
+  return {
+    config,
+    migrated
+  }
+}
+
+export const getDefaultAuthConfig = async (existing: { token: string }) => {
+  let migrated = false
+  const config: {
+    _: string
+    token?: string
+  } = {
+    _: 'This is 🐟 cli credentials file. DO NOT SHARE!'
+  }
+
+  if (existing) {
+    try {
+      config.token = existing.token
+      migrated = true
+    } catch (err) {}
+  }
+
+  return {
+    config,
+    migrated
+  }
+}
